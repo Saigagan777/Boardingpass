422df05c3a16f2d16cb449f3a61892ef2aa5cc5f
rakesh-kondasani <rakeshk@karncy.com>
2026-07-13 19:23:40 +0530

    new changes

--- lib/utils/app_logo.dart
+++ lib/utils/app_logo.dart
@@ -18,21 +18,38 @@ class AppLogo extends StatelessWidget {
   Widget build(BuildContext context) {
     final primary = color ?? const Color(0xFF3E1F11);
 
+    final logoIcon = Image.asset(
+      'assets/images/app_logo.png',
+      width: size,
+      height: size,
+      fit: BoxFit.contain,
+    );
+
     if (!showText) {
-      return const SizedBox.shrink();
+      return logoIcon;
     }
 
-    return Text(
-      'NexMeet',
-      maxLines: 1,
-      overflow: TextOverflow.ellipsis,
-      style: TextStyle(
-        fontFamily: 'PlayfairDisplay',
-        fontSize: size * 0.72,
-        fontWeight: FontWeight.w800,
-        letterSpacing: -0.5,
-        color: primary,
-      ),
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        logoIcon,
+        SizedBox(width: size * 0.3),
+        Flexible(
+          child: Text(
+            'NexMeet',
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+            style: TextStyle(
+              fontFamily: 'PlayfairDisplay',
+              fontSize: size * 0.72,
+              fontWeight: FontWeight.w800,
+              letterSpacing: -0.5,
+              color: primary,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
